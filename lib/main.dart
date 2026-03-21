08f3489010319118cbe6fead6a594e4b0958bdd4
Krystof <krystof@mikulkovi.cz>
2026-03-21 23:44:18 +0100

    maly_zmeny_freshstart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() {
+  runApp(const MyApp());
+}
 
-  home: Text("loooooool"),
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
 
 
-)); 
+
+    );
+      
+  }
+
+
+
+}
