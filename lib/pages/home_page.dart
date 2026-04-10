d089679ffc353887d3f2ff637c0aaf6222a895fe
Krystof Mikulka <1254@student.itg.cz>
2026-04-10 23:56:56 +0200

    goingback

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,58 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/utilities/todo_tile.dart';
 
-
-class HomePage extends StatefulWidget {
-
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-    //list of to do tasks
-    List toDoList = [
-    ["goon", false],
-    ["sybau", true],
-  ];
-
-//checkbox was tapped
-void checkboxChanged(bool? value, int index) {
-  setState(() {
-    toDoList[index][1] = !toDoList[index][1];
-  });
-}
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow[200],
 
+      backgroundColor: Colors.yellow[200],
       appBar: AppBar(
-        backgroundColor: Colors.yellow,
-        title: Text("To do"),
-        centerTitle: true,
+        backgroundColor: Colors.yellow[500],
       ),
 
 
-      body: ListView.builder(
-        itemCount: toDoList.length,
-        itemBuilder: (context, index) {
-          return
-          TodoTile(
-            taskName: toDoList[index][0],
-            taskCompleted: toDoList[index][1],
-            onChanged: (value) => checkboxChanged(value, index),
-            );
-        },
-      )
 
 
-
-    ); 
+    );
   }
 }
