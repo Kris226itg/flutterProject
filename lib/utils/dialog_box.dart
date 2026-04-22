3b41c4f5e0c744c55c03ab36f834b7d1975903d2
Krystof Mikulka <1254@student.itg.cz>
2026-04-22 23:54:01 +0200

    progres

--- lib/utils/dialog_box.dart
+++ lib/utils/dialog_box.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/my_button.dart';
 
 class DialogBox extends StatelessWidget {
   const DialogBox({super.key});
 
   @override
+
+void saveNewTask(){
+  
+}
+
+
+
+
   Widget build(BuildContext context) {
     return AlertDialog(
       backgroundColor: Colors.yellow,
@@ -12,6 +21,7 @@ class DialogBox extends StatelessWidget {
         width: 300,
         
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             //user input
             TextField(decoration: InputDecoration(
@@ -22,7 +32,13 @@ class DialogBox extends StatelessWidget {
             
 
             //buttons save + cancel
-
+            Row(
+              children: [
+                MyButton(text: "save", onPressed: (){}),
+                MyButton(text: "cancel", onPressed: (){})
+              ],
+              
+            )
 
 
           ],
